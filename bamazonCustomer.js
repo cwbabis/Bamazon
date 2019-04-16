138dcc57569645a1401f87c4b64f11de31f267ea
Babis <cwbabis@gmail.com>
2019-04-16 00:10:55 -0700

    Add Inventory Function Works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,7 +36,7 @@ function runApp() {
     .then(function (answer) {
       switch (answer.action) {
         case "View Products for Sale":
-          productList();
+          inventoryList();
           break;
 
         case "View Low Inventory":
@@ -48,7 +48,7 @@ function runApp() {
           break;
 
         case "Add New Product":
-          productNew();
+          inventoryNew();
           break;
 
         case "Exit":
@@ -56,4 +56,93 @@ function runApp() {
           break;
       }
     });
+}
+
+function inventoryList() {
+  inquirer
+    .prompt({
+      name: "inventory",
+      type: "list",
+      message: "List of Available Products:",
+      choices: [
+
+      ]
+    })
+    .then(function (answer) {
+      var query = "SELECT * FROM bamazon";
+
+      //(property) Connection.query: QueryFunction
+      //(options: string | QueryOptions, callback?: queryCallback) => Query (+2 overloads)
+      connection.query(query, { inventory: answer.inventory }, function (error, response) {
+        for (var i = 0; i < response.length; i++) {
+          console.log("Position: " + response[i].position + " || Song: " + response[i].song + " || Year: " + response[i].year);
+        }
+        runApp();
+      });
+    });
+}
+function inventoryLow() {
+
+}
+function inventoryAdd() {
+  inquirer
+    .prompt([
+      {
+        name: "item"
+      }
+    ])
+}
+function inventoryNew() {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What item would you like to add to the list of inventory?"
+      },
+      {
+        name: "department",
+        type: "input",
+        message: "What department does this item belong to?"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "How much does this item cost?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+      name: "stock",
+        type: "input",
+        message: "How much of this item are you into the inventory?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer){
+      connection.query(
+        "INSERT INTO products SET ?",
+        {
+          product_name: answer.item,
+          department_name: answer.department,
+          price: answer.price,
+          stock_quantity: answer.stock
+        },
+        function(error){
+          if (error) throw err;
+          console.log("Your item was successfully added to the current list inventory!")
+          runApp();
+        }
+      )
+    })
+
 }
